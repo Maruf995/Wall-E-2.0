76af66137c24c6f320803260c03521c47dd2704b
Maruf995git <mamadalievmaruf740@gmail.com>
2025-03-01 11:00:33 +0600

    First commit

---
+++ main.py
@@ -0,0 +1,32 @@
+import openai
+
+def main():
+    client = openai.OpenAI(
+        api_key="6ac30328-5a39-4e77-931e-82e02f14f073",
+        base_url="https://api.sambanova.ai/v1",
+    )
+    
+    while True:
+        user_input = input("Введите ваш запрос: ")
+        if user_input.lower() in ["выход", "exit", "quit"]:
+            print("Выход из программы.")
+            break
+        
+        try:
+            response = client.chat.completions.create(
+                model='Meta-Llama-3.1-8B-Instruct',
+                messages=[
+                    {"role": "system", "content": "Your name is Валли! You are a robot exhibit at a science museum. You help children learn new things. But briefly."},
+                    {"role": "user", "content": user_input}
+                ],
+                temperature=0.1,
+                top_p=0.1
+            )
+            
+            answer = response.choices[0].message.content
+            print("Валли:", answer)
+        except Exception as e:
+            print(f"Ошибка взаимодействия с OpenAI: {e}")
+
+if __name__ == "__main__":
+    main()
